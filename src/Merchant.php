53c4db7a10d2f8678bdcaaf3ea476f0083d9eea8
Andrii Vasyliev <sol@hiqdev.com>
2015-10-22 14:40:27 +0000

    inited

---
+++ src/Merchant.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * PHP merchant
+ *
+ * @link      https://github.com/hiqdev/php-merchant
+ * @package   php-merchant
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (https://hiqdev.com/)
+ */
+
+namespace hiqdev\php\merchant\webmoney;
+
+class MoneyMerchant extends \hiqdev\php\merchant\webmoney\Merchant
+{
+    protected static $_defaults = [
+        'name'          => 'webmoney',
+        'label'         => 'WebMoney',
+        'actionUrl'     => 'https://merchant.webmoney.ru/lmi/payment.asp',
+        'confirmText'   => 'OK'
+    ];
+
+    public function getInputs()
+    {
+        return [
+            'LMI_PAYMENT_DESC'      => $this->description,
+            'LMI_PAYEE_PURSE'       => $this->purse,
+            'LMI_PAYMENT_AMOUNT'    => $this->total,
+            'LMI_RESULT_URL'        => $this->confirmUrl,
+            'LMI_SUCCESS_URL'       => $this->successUrl,
+            'LMI_FAIL_URL'          => $this->failureUrl,
+            'LMI_SUCCESS_METHOD'    => 1,
+            'LMI_FAIL_METHOD'       => 1,
+        ];
+    }
+
+    public function validateConfirmation($data)
+    {
+    }
+}
