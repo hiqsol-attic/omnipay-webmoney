5dde0373c3e621877e5314118d9c6b9873375e2c
Andrii Vasyliev <sol@hiqdev.com>
2015-11-04 08:21:15 +0000

    + test data for webmoney

---
+++ tests/test_data.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * WebMoney plugin for PHP merchant library
+ *
+ * @link      https://github.com/hiqdev/php-merchant-webmoney
+ * @package   php-merchant-webmoney
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2015, HiQDev (http://hiqdev.com/)
+ */
+
+$data = [
+    'LMI_MODE'             => '0',
+    'LMI_PAYMENT_AMOUNT'   => '1.01',
+    'LMI_PAYEE_PURSE'      => 'Z348367294853',
+    'LMI_PAYMENT_NO'       => '0',
+    'LMI_PAYER_WM'         => '898472048237',
+    'LMI_PAYER_PURSE'      => 'Z847392391238',
+    'LMI_PAYER_COUNTRYID'  => 'OL',
+    'LMI_PAYER_PCOUNTRYID' => 'OL',
+    'LMI_PAYER_IP'         => '34.24.14.104',
+    'LMI_SYS_INVS_NO'      => '365495932',
+    'LMI_SYS_TRANS_NO'     => '3649346729',
+    'LMI_SYS_TRANS_DATE'   => '20151101 23:05:56',
+    'LMI_HASH'             => '2A24482DD6BBB8D0AE1D3C1801B77252',
+    'LMI_PAYMENT_DESC'     => 'Test payment description',
+    'LMI_LANG'             => 'ru-RU',
+    'LMI_DBLCHK'           => 'ENUM',
+];
